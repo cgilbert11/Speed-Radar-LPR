0a51e24034a03d52c01d8aa7922ef02903d56774
Colby Gilbert <92291342+cgilbert11@users.noreply.github.com>
2022-04-11 16:32:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
 
+echo "Installing Updates"
+sudo apt update
+sudo apt upgrade
+
+echo "Installing pyserial"
+pip install pyserial
+
+echo "Installing xlwt"
+pip install xlwt
+
+echo "Installing putty"
+sudo apt-get install putty -y
